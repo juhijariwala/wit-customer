c6770e31633837e258412e242d12047d2e034e8c
Swarnava Mukherjee <swarnava.mukherjee@thoughtworks.com>
2018-12-04 12:54:22 +0530

    Modified HTTP request mappings to respective CRUD operations

--- src/main/java/com/wit/customer/controller/CustomerController.java
+++ src/main/java/com/wit/customer/controller/CustomerController.java
@@ -9,22 +9,22 @@ import org.springframework.web.bind.annotation.*;
 public class CustomerController {
 
     @GetMapping("{id}")
-    public ResponseEntity doGet(@PathVariable Long id) {
+    public ResponseEntity doGet(@PathVariable long id) {
         return ResponseEntity.ok().body(id + new CustomerService().toString());
     }
 
-    @PostMapping("create/{id}")
-    public ResponseEntity doCreate(@PathVariable Long id) {
+    @PostMapping("create")
+    public ResponseEntity doCreate() {
+        return ResponseEntity.ok().body(new CustomerService().toString());
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity doUpdate(@PathVariable long id) {
         return ResponseEntity.ok().body(id + new CustomerService().toString());
     }
 
-    @PostMapping("update/{id}")
-    public ResponseEntity doUpdate(@PathVariable String id) {
-        return ResponseEntity.ok().body(id + new CustomerService().toString());
-    }
-
-    @PostMapping("delete/{id}")
-    public ResponseEntity doDelete(@PathVariable Long id) {
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity doDelete(@PathVariable long id) {
         return ResponseEntity.ok().body(id + new CustomerService().toString());
     }
 
