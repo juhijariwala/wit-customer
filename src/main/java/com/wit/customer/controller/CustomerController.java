b8979fa767cf7d761bdb8e36668ec9fcb4ac815f
Swarnava Mukherjee <swarnava.mukherjee@thoughtworks.com>
2018-12-10 12:55:40 +0530

    Autowire Customer Service

--- src/main/java/com/wit/customer/controller/CustomerController.java
+++ src/main/java/com/wit/customer/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.wit.customer.controller;
 
 import com.wit.customer.model.Customer;
 import com.wit.customer.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,24 +10,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/customer/")
 public class CustomerController {
 
+    @Autowired
+    private CustomerService service;
+
     @GetMapping("{id}")
     public ResponseEntity doGet(@PathVariable long id) {
-        return ResponseEntity.ok().body(new CustomerService().get(id));
+        return ResponseEntity.ok().body(service.get(id));
     }
 
     @PostMapping("create")
     public ResponseEntity doCreate(@ModelAttribute Customer customer) {
-        return ResponseEntity.ok().body(new CustomerService().create(customer));
+        return ResponseEntity.ok().body(service.create(customer));
     }
 
     @PutMapping("{id}")
     public ResponseEntity doUpdate(@ModelAttribute Customer customer) {
-        return ResponseEntity.ok().body(new CustomerService().update(customer));
+        return ResponseEntity.ok().body(service.update(customer));
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity doDelete(@PathVariable long id) {
-        return ResponseEntity.ok().body(new CustomerService().delete(id));
+        return ResponseEntity.ok().body(service.delete(id));
     }
 
 }
